0b8a6c4afd2c9f9c1eff0d54be5eb9a57cf276d6
Ren <rg326@live.com>
2026-04-10 23:54:29 -0400

    added comments to App component

--- react-counter/src/App.tsx
+++ react-counter/src/App.tsx
@@ -5,7 +5,8 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0) //initiate as number bc its a counter
+  // same as let count = 0
 
   return (
     <>
