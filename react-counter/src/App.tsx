a101ecaa3367e50bc1edbf15c0bb7dcd4e0f42ae
Ren <rg326@live.com>
2026-04-16 23:17:40 -0400

    minor edit

--- react-counter/src/App.tsx
+++ react-counter/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 //refactor into props comp practice
 //hero ui breakdown
 // counter breakdown
-//refactor to react native (later)
+//refactor to react native using expo (later)
   return (
     <>
       <section id="center">
