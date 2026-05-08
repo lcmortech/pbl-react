7341bba705ca5398cf61f684480dc5b68eca957f
Ren <rg326@live.com>
2026-05-07 23:57:30 -0400

    added bluesky link to App.tsx

--- react-counter/src/App.tsx
+++ react-counter/src/App.tsx
@@ -73,6 +73,18 @@ const AppPrac = () =>{
                 X.com
               </a>
             </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
             <ul>
         </div>
       </section>
