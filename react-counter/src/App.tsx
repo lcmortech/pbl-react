dbe805e183a79cb701247412ee44f5acd279962f
Ren <rg326@live.com>
2026-05-13 23:48:59 -0400

    renamed variables

--- react-counter/src/App.tsx
+++ react-counter/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const App = () =>{
-const [count, setCount] = useState(0)
+   // [Current State, Setter Function] = useState(Initial State)
+const [counter, setCounter] = useState(0)
+const increment = () => setCounter(counter + 1)
+const decrement = () => setCounter(counter -1)
 
 return(
-  <h1>Counter: {count}</h1>
+  <>
+    <h1>Counter: {counter}</h1>
+    <button onClick={increment}>Increase</button>
+    <button onClick={decrement}>Decrease</button>
+  </>
 )
-
 }
+
+export default App
