90ea92d0dbf9f9c1e7cd7c590fe1640c369e6721
Ren <rg326@live.com>
2026-05-18 23:57:17 -0400

    added import usestate to ttt

--- react-ttt/tic-tac-toe/src/App.tsx
+++ react-ttt/tic-tac-toe/src/App.tsx
@@ -1,18 +1,1 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Tic-Tac-Toe Game (React Docs)</h1>
-      {/* add grid */}
-    </>
-  )
-}
-
-export default App
+import {useState} from 'react';
