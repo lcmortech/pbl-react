b58e2963da867cca81d78f69bc096d096099522b
Ren <rg326@live.com>
2026-05-21 23:59:17 -0400

    added example to react-ttt

--- react-ttt/tic-tac-toe/src/App.tsx
+++ react-ttt/tic-tac-toe/src/App.tsx
@@ -3,3 +3,12 @@ import {useState} from 'react';
 export default function Square() {
   return <button className="square">X</button>
 }
+
+// export default function Square() {
+//   return (
+//     <>
+//       <button className="square">X</button>
+//       <button className="square">X</button>
+//     </>
+//   );
+// }
