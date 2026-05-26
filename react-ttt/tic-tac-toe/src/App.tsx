52304bfbe9d455f67f6c148ea9ed4f463761695a
Ren <rg326@live.com>
2026-05-25 23:54:51 -0400

    added square components to each board row in react-ttt

--- react-ttt/tic-tac-toe/src/App.tsx
+++ react-ttt/tic-tac-toe/src/App.tsx
@@ -10,8 +10,18 @@ export default function Board(){
   return (
     <div className="board-row">
       <Square />
+      <Square />
+      <Square />
     </div>
-    <div className="board-row"></div>
-    <div className="board-row"></div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
   )
 }
