48c1b826118566167abdd523e74c6ab1b7ade7fb
Ren <rg326@live.com>
2026-05-26 23:47:08 -0400

    added fragment to Board component in react-ttt

--- react-ttt/tic-tac-toe/src/App.tsx
+++ react-ttt/tic-tac-toe/src/App.tsx
@@ -8,20 +8,22 @@ function Square(){
 
 export default function Board(){
   return (
-    <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
-    </div>
-    <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
-    </div>
-    <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
-    </div>
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
   )
 }
