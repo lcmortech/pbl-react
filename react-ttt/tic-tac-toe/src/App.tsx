9a28269298fa89aa209a2189e3cb73205b88f585
Ren <rg326@live.com>
2026-05-27 23:49:09 -0400

    added square values to board in react-ttt

--- react-ttt/tic-tac-toe/src/App.tsx
+++ react-ttt/tic-tac-toe/src/App.tsx
@@ -10,14 +10,14 @@ export default function Board(){
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
       </div>
       <div className="board-row">
         <Square />
