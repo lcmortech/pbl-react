2c4cf6f16ea61be14d06a9a957094383df41acac
Ren <rg326@live.com>
2026-06-06 23:49:57 -0400

    readded board component to react-ttt

--- react-ttt/tic-tac-toe/src/App.tsx
+++ react-ttt/tic-tac-toe/src/App.tsx
@@ -8,3 +8,16 @@ function Square({ value, onSquareClick }) {
   );
 }
 
+function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    onPlay(nextSquares);
+  }
