786a5996e1965f08f2185dc995e1a580f6b601ad
Ren <rg326@live.com>
2026-06-07 23:46:19 -0400

    step 6 - building the board in react-ttt

--- react-ttt/tic-tac-toe/src/App.tsx
+++ react-ttt/tic-tac-toe/src/App.tsx
@@ -1,4 +1,10 @@
 //3 - Setup for the tutorial
+//6 - Building the board
 export default function Square(){
-  return <button className="square">X</button>
+  return( 
+    <>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    </>
+  )
 }
