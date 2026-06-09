60bda02afc05e448f5ac0635e96a11d0289c4787
Ren <rg326@live.com>
2026-06-08 23:58:31 -0400

    replaced x with numbers in react-ttt

--- react-ttt/tic-tac-toe/src/App.tsx
+++ react-ttt/tic-tac-toe/src/App.tsx
@@ -3,8 +3,8 @@
 export default function Square(){
   return( 
     <>
-    <button className="square">X</button>
-    <button className="square">X</button>
+    <button className="square">1</button>
+    <button className="square">2</button>
     </>
   )
 }
