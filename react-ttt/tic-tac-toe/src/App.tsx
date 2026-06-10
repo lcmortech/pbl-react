ec5c4b5e1e6207477c7ab575573e90e4f260793b
Ren <rg326@live.com>
2026-06-09 23:56:15 -0400

    changed classname of div to board-row in react-ttt

--- react-ttt/tic-tac-toe/src/App.tsx
+++ react-ttt/tic-tac-toe/src/App.tsx
@@ -2,10 +2,10 @@
 //6 - Building the board
 export default function Board(){
   return( 
-    <>
-    <button className="square">1</button>
-    <button className="square">2</button>
-    <button className="square">3</button>
-    </>
+    <div className="board-row">
+      <button className="square">1</button>
+      <button className="square">2</button>
+      <button className="square">3</button>
+    </div>
   )
 }
