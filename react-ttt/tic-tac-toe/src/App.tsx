01bd16c923ab3a19013a17a58eff1c52671050f6
Ren <rg326@live.com>
2026-06-14 23:29:25 -0400

    added setValue() to function handleClick in react-ttt

--- react-ttt/tic-tac-toe/src/App.tsx
+++ react-ttt/tic-tac-toe/src/App.tsx
@@ -6,10 +6,10 @@ import {useState} from 'react'
 //8 - Making an interactive component
 function Square(){
 
-  const [value, setValue] = useState(0)
+  const [value, setValue] = useState(null)
 
   function handleClick(){
-    console.log('clicked!')
+    setValue('X')
   }
   return (<button className="square" onClick={handleClick}>{value}</button>)
 }
