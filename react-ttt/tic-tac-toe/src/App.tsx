448c3bbcad4e2bb34c9f90fd4b685a14a3ff4882
Ren <rg326@live.com>
2026-06-15 23:57:11 -0400

    replaced square components in react-ttt

--- react-ttt/tic-tac-toe/src/App.tsx
+++ react-ttt/tic-tac-toe/src/App.tsx
@@ -17,19 +17,19 @@ export default function Board(){
   return( 
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="8"/>
-        <Square value="9"/>
-        <Square value="10" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       </>
   )
