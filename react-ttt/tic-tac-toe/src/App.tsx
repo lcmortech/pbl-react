2581a8b2617d250febcf752c15d78222080bf9a8
Ren <rg326@live.com>
2026-06-17 23:56:47 -0400

    added values to square array in react-ttt

--- react-ttt/tic-tac-toe/src/App.tsx
+++ react-ttt/tic-tac-toe/src/App.tsx
@@ -19,19 +19,19 @@ export default function Board(){
   return( 
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[0]}/>
+        <Square value={square[1]}/>
+        <Square value={square[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[3]}/>
+        <Square value={square[4]}/>
+        <Square value={square[5]/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[6]}/>
+        <Square value={square[7]}/>
+        <Square value={square[8]}/>
       </div>
       </>
   )
