1a0d3dcefc6c894dbd238d3939197c67254989d2
Ren <rg326@live.com>
2026-07-08 23:56:00 -0400

    added progress notes to react-ttt

--- react-ttt/tic-tac-toe/src/ex-App.tsx
+++ react-ttt/tic-tac-toe/src/ex-App.tsx
@@ -14,6 +14,7 @@ import {useState} from 'react';
 //Adding time travel
 //Storing a history of moves
 //Lifting state up, again
+//Showing the past moves
 // fix errors (nextSquares)
 //4th of july break
 function Square({value, onSquareClick}){
