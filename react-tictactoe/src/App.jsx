f6650c51be964645af73b6256dc006f2df20373c
Ren <rg326@live.com>
2026-07-11 23:50:33 -0400

    added first board row div

--- react-tictactoe/src/App.jsx
+++ react-tictactoe/src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 
 export default function Board() {
   return (
-    <div>
-        <button className="square"></button>
+    <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
     </div>
   )
 }
