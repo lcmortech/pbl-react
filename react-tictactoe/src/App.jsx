e81ee94c26caaf393ab71560e9d7f04a0612b1ea
Ren <rg326@live.com>
2026-07-11 23:49:52 -0400

    added divs

--- react-tictactoe/src/App.jsx
+++ react-tictactoe/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
-export default function Square() {
-  return <button className="square">X</button>;
+export default function Board() {
+  return (
+    <div>
+        <button className="square"></button>
+    </div>
+  )
 }
