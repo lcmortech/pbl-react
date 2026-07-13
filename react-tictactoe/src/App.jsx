7cc72dce8e4a818f2a5e2445586297ff121a9849
Ren <rg326@live.com>
2026-07-12 23:57:31 -0400

    added extra board row divs

--- react-tictactoe/src/App.jsx
+++ react-tictactoe/src/App.jsx
@@ -7,5 +7,15 @@ export default function Board() {
         <button className="square">2</button>
         <button className="square">3</button>
     </div>
+    <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+    </div>
+    <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+    </div>
   )
 }
