486b86ae61623283038468e65f7cd6e224d15348
Ren <rg326@live.com>
2026-07-15 23:59:42 -0400

    added value/setvalue hooks to square component

--- react-tictactoe/src/App.jsx
+++ react-tictactoe/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
 export default function Square({value}){
+
+  const [value, setValue] = useState(null)
       function handleClick() {
     console.log('clicked!');
   }
