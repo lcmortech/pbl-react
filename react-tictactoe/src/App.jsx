1b2152f311f3ec64558d369d5e28961065a7527e
Ren <rg326@live.com>
2026-07-17 23:58:29 -0400

    removed value props from squares in board component

--- react-tictactoe/src/App.jsx
+++ react-tictactoe/src/App.jsx
@@ -14,24 +14,24 @@ export default function Square({value}){
 
 
 
-
+//Each Square has its own state: the value stored in each Square is completely independent of the others. When you call a set function in a component, React automatically updates the child components inside too.
 function Board() {
   return (
     <>
     <div className="board-row">
-        <Square value="1" />
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square />
+        <Square />
+        <Square />
     </div>
     <div className="board-row">
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square />
+        <Square />
+        <Square />
     </div>
     <div className="board-row">
-        <Square value="7"/>
-        <Square value="8"/>
-        <Square value="9"/>
+        <Square />
+        <Square />
+        <Square />
     </div>
     </>
   )
