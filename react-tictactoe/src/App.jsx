d4b0964e1d2cea6ecd1cb091928c4aeb022ade07
Ren <rg326@live.com>
2026-07-19 23:44:48 -0400

    added event handler test to square component

--- react-tictactoe/src/App.jsx
+++ react-tictactoe/src/App.jsx
@@ -26,7 +26,7 @@ function Board() {
   return (
     <>
     <div className="board-row">
-        <Square value={squares[0]}/>
+        <Square value={squares[0]} onSquareClick={() => handleClick(1)}/>
         <Square value={squares[1]}/>
         <Square value={squares[2]}/>
     </div>
