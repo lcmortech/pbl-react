c2e97245840140a79204b8291e4adeaf16055f81
Ren <rg326@live.com>
2026-07-22 23:55:07 -0400

    added calculatewinner function to board component

--- react-tictactoe/src/App.jsx
+++ react-tictactoe/src/App.jsx
@@ -30,6 +30,19 @@ function Board() {
     setXIsNext(xIsNext);
   }
 
+  
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
   return (
     <>
     <div className="board-row">
