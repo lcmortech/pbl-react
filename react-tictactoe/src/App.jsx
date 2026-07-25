069aec72623e09e68948f4267398f7dcbc5ba8d7
Ren <rg326@live.com>
2026-07-24 23:58:43 -0400

    Added hooks to game component

--- react-tictactoe/src/App.jsx
+++ react-tictactoe/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 
 export default function Game() {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+
   return (
     <div className="game">
       <div className="game-board">
