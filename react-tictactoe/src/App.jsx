dc3704e8a1efe32853a995580ad544cf7fc766a7
Ren <rg326@live.com>
2026-07-25 23:53:24 -0400

    added variable currentsquares to game component

--- react-tictactoe/src/App.jsx
+++ react-tictactoe/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function Game() {
     const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
+    const currentSquares = history[history.length - 1];
 
   return (
     <div className="game">
