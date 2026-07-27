66dcecaa3ab577d6e4c23ae4f0dfdd749502b9d7
Ren <rg326@live.com>
2026-07-26 23:53:15 -0400

    added props to board instance in game component

--- react-tictactoe/src/App.jsx
+++ react-tictactoe/src/App.jsx
@@ -8,11 +8,11 @@ export default function Game() {
       function handlePlay(nextSquares) {
     // TODO
   }
-  
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{/*TODO*/}</ol>
