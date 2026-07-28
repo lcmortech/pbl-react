4f938879b00d55a64ec5110b3d417e85d6ac6061
Ren <rg326@live.com>
2026-07-27 23:53:42 -0400

    modified func handleplay

--- react-tictactoe/src/App.jsx
+++ react-tictactoe/src/App.jsx
@@ -5,8 +5,9 @@ export default function Game() {
     const [history, setHistory] = useState([Array(9).fill(null)]);
     const currentSquares = history[history.length - 1];
 
-      function handlePlay(nextSquares) {
-    // TODO
+        function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
   }
 
   return (
