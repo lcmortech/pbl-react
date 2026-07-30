8b0a743877e1eff5df8098bab67197d92e4d8937
Ren <rg326@live.com>
2026-07-29 23:58:55 -0400

    added hook for current moves in game component

--- react-tictactoe/src/App.jsx
+++ react-tictactoe/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function Game() {
     const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
     const currentSquares = history[history.length - 1];
 
         function handlePlay(nextSquares) {
