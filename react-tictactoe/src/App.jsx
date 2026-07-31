23905021018b40b03fced9e9277f17ce40a4a8c3
Ren <rg326@live.com>
2026-07-30 23:57:51 -0400

    added key to li for move

--- react-tictactoe/src/App.jsx
+++ react-tictactoe/src/App.jsx
@@ -23,9 +23,10 @@ export default function Game() {
       description = 'Go to game start';
     }
     return (
-      <li>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
+    <li key={move}>
+      <button onClick={() => jumpTo(move)}>{description}</button>
+    </li>
+      //
     );
   });
 
